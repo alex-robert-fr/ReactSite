dfad0f418363a497b5e97664145e090186c20c23
Alex Robert <contact.alexrobert04@gmail.com>
2021-11-06 16:39:32 +0100

    [ROUTER] - Add differents projects page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import { Redirect, Route, Switch } from 'react-router'
-import Contact from './pages/Contact'
-import Home from './pages/Home'
-import Portfolio from './pages/Portfolio'
+import Contact from './pages/Contact.jsx'
+import Home from './pages/Home.jsx'
+import { Project1, Project2, Project3, Project4 } from './pages/Projects'
 
 function App() {
   return (
     <Switch>
       <Route exact path="/" component={Home} />
-      <Route exact path="/project-1" component={Portfolio} />
+      <Route exact path="/project-1" component={Project1} />
+      <Route exact path="/project-2" component={Project2} />
+      <Route exact path="/project-3" component={Project3} />
+      <Route exact path="/project-4" component={Project4} />
       <Route exact path="/contact" component={Contact} />
       <Redirect to="/" />
     </Switch>
---
+++ src/pages/Projects.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+export function Project1() {
+    return (
+        <main>
+            <div className="project">
+                Project 1
+            </div>
+        </main>
+    )
+}
+export function Project2() {
+    return (
+        <main>
+            <div className="project">
+                Project 2
+            </div>
+        </main>
+    )
+}
+export function Project3() {
+    return (
+        <main>
+            <div className="project">
+                Project 3
+            </div>
+        </main>
+    )
+}
+export function Project4() {
+    return (
+        <main>
+            <div className="project">
+                Project 4
+            </div>
+        </main>
+    )
+}
