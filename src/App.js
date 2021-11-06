2f8e905f4f92412d1c06739ad375281f43afceb7
Alex Robert <contact.alexrobert04@gmail.com>
2021-11-06 16:29:20 +0100

    [ROUTER] - Add Routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import { Route, Switch } from 'react-router'
+import Contact from './pages/Contact'
+import Home from './pages/Home'
+import Portfolio from './pages/Portfolio'
 
 function App() {
   return (
-    <div>
-      <p>App</p>
-    </div>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/project-1" component={Portfolio} />
+      <Route exact path="/contact" component={Contact} />
+    </Switch>
   )
 }
 
