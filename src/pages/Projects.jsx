c75da9ef5017695712c71643486a08c6eb0d7ac8
Alex Robert <contact.alexrobert04@gmail.com>
2021-11-06 17:59:06 +0100

    [FRONT] - Add Button Bottom in all Project Page

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import ButtonsBottom from '../components/ButtonsBottom'
+import Navigation from '../components/Navigation'
 
 export function Project1() {
     return (
         <main>
             <div className="project">
+                <Navigation />
                 Project 1
+                <ButtonsBottom left={'/'} right={'/project-2'} />
             </div>
         </main>
     )
@@ -13,7 +17,9 @@ export function Project2() {
     return (
         <main>
             <div className="project">
+                <Navigation />
                 Project 2
+                <ButtonsBottom left={'/project-1'} right={'/project-3'} />
             </div>
         </main>
     )
@@ -22,7 +28,9 @@ export function Project3() {
     return (
         <main>
             <div className="project">
+                <Navigation />
                 Project 3
+                <ButtonsBottom left={'/project-2'} right={'/project-4'} />
             </div>
         </main>
     )
@@ -31,7 +39,9 @@ export function Project4() {
     return (
         <main>
             <div className="project">
+                <Navigation />
                 Project 4
+                <ButtonsBottom left={'/project-3'} right={'/contact'} />
             </div>
         </main>
     )
