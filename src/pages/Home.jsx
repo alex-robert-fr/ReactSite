ce48a6ad81ac4a89d44975a114e59e84cde3bb69
Alex Robert <contact.alexrobert04@gmail.com>
2021-11-06 17:40:12 +0100

    [FRONT] - Add Dynamic Text HomePage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
+import DynamicText from '../components/DynamicText'
 import Navigation from '../components/Navigation'
 import SocialNetwork from '../components/SocialNetwork'
 
 function Home() {
     return (
-        <div>
-            <Navigation />
-            <SocialNetwork />
+        <div className="home">
+            <Navigation/>
+            <SocialNetwork/>
+            <div className="home-main">
+                <div className="main-content">
+                    <h1>FS AGENCY</h1>
+                    <h2><DynamicText /></h2>
+                </div>
+            </div>
         </div>
     )
 }
